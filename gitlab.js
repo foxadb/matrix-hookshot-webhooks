d764ebe67d603991716a50f7db7f5df62e34f98e
Samy Amraoui <samy.amraoui@amundi.com>
2025-12-24 20:41:35 +0100

    feat: print error when gitlab js transformation fails

--- gitlab.js
+++ gitlab.js
@@ -5,44 +5,50 @@ if (data === undefined) {
     return;
 }
 
-const user = data.user_name ?? data.user.name;
-const username = data.user_username ?? data.user.username;
-const projectName = data.project.name;
-const projectUrl = data.project.web_url;
-const mergeRequest = data.merge_request;
-const ref = typeof data.ref === 'string' ? data.ref.split('/')[2] : undefined;
-
 let html, plain;
 
-if (data.object_kind === 'merge_request') {
-    const { action, title, url } = data.object_attributes;
-    plain = `${user} (${username}) ${action} [${title}](${url}) in [${projectName}](${projectUrl})`;
-} else if (data.object_kind === 'note') {
-    const note = data.object_attributes.note;
-    const url = data.object_attributes.url;
-    html = `<p>${user} (${username}) <a href="${url}">commented</a> in <a href="${projectUrl}">${projectName}</a>`;
-    plain = `${user} (${username}) [commented](${url}) in [${projectName}](${projectUrl})`;
-    if (mergeRequest) {
-        html += `: ${mergeRequest.title}</p>`;
-        plain += `: ${mergeRequest.title}`;
-    } else {
-        html += '</p>';
+try {
+    const user = data.user_name ?? data.user.name;
+    const username = data.user_username ?? data.user.username;
+    const projectName = data.project.name;
+    const projectUrl = data.project.web_url;
+    const mergeRequest = data.merge_request;
+    const ref = typeof data.ref === 'string' ? data.ref.split('/')[2] : undefined;
+
+    if (data.object_kind === 'merge_request') {
+        const { action, title, url } = data.object_attributes;
+        plain = `${user} (${username}) ${action} [${title}](${url}) in [${projectName}](${projectUrl})`;
+    } else if (data.object_kind === 'note') {
+        const note = data.object_attributes.note;
+        const url = data.object_attributes.url;
+        html = `<p>${user} (${username}) <a href="${url}">commented</a> in <a href="${projectUrl}">${projectName}</a>`;
+        plain = `${user} (${username}) [commented](${url}) in [${projectName}](${projectUrl})`;
+        if (mergeRequest) {
+            html += `: ${mergeRequest.title}</p>`;
+            plain += `: ${mergeRequest.title}`;
+        } else {
+            html += '</p>';
+        }
+        html += `\n<blockquote>\n<p>${note}</p>\n</blockquote>`;
+    } else if (data.object_kind === 'push') {
+        const lastCommit = data.commits[data.commits.length - 1];
+        const { id, title, url } = lastCommit;
+        const shortId = id.slice(0, 8);
+        plain = `${user} (${username}) pushed to branch [${ref}](${url}) of [${projectName}](${projectUrl})`;
+        plain += `\n> [${shortId}](${url}): ${title}`;
+    } else if (data.object_kind === 'tag_push') {
+        let action = 'deleted tag';
+        let url = `${projectUrl}/-/tags`;
+        if (typeof data.checkout_sha === 'string') {
+            action = 'pushed new tag';
+            url += `/${ref}`;
+        }
+        plain = `${user} (${username}) ${action} [${ref}](${url}) to [${projectName}](${projectUrl})`;
     }
-    html += `\n<blockquote>\n<p>${note}</p>\n</blockquote>`;
-} else if (data.object_kind === 'push') {
-    const lastCommit = data.commits[data.commits.length - 1];
-    const { id, title, url } = lastCommit;
-    const shortId = id.slice(0, 8);
-    plain = `${user} (${username}) pushed to branch [${ref}](${url}) of [${projectName}](${projectUrl})`;
-    plain += `\n> [${shortId}](${url}): ${title}`;
-} else if (data.object_kind === 'tag_push') {
-    let action = 'deleted tag';
-    let url = `${projectUrl}/-/tags`;
-    if (typeof data.checkout_sha === 'string') {
-        action = 'pushed new tag';
-        url += `/${ref}`;
-    }
-    plain = `${user} (${username}) ${action} [${ref}](${url}) to [${projectName}](${projectUrl})`;
+} catch (error) {
+    plain = 'Webhook received but failed to process via transformation function\n\n';
+    plain += error.stack.split(" at VM2 Wrapper.apply")[0];
+    plain += '```json\n' + JSON.stringify(data, null, 2) + '\n```';
 }
 
 result = {
